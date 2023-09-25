189d5c401cc7c5537bc30d2ed709ac43051346a8
Shankhapriya <shankhapriya2015@gmail.com>
2023-09-25 13:53:18 +0530

    CreateNewProject

---
+++ View/LogInSignUp/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LogInSignUp = () => {
+  return (
+    <View>
+      <Text>LogInSignUp</Text>
+    </View>
+  )
+}
+
+export default LogInSignUp
---
+++ View/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const mainRoute = () => {
+  return (
+    <View>
+      <Text>mainRoute</Text>
+    </View>
+  )
+}
+
+export default mainRoute
